ca98a09bf46e2e6e2eb84f3205280bd216e76fe9
Philipp Kunz <philippkunz@MBP-von-Philipp.fritz.box>
2025-05-14 18:48:22 +0200

    GPU memory growth configuration changed.

--- Code/experiments/experiment_2_dipe_insertion_sort_8096_ablation_bottleneck.py
+++ Code/experiments/experiment_2_dipe_insertion_sort_8096_ablation_bottleneck.py
@@ -198,6 +198,10 @@ class DIPEInsertionSortSearch(HyperoptBayesianSearch):
 
         return [fine_tuning]
 
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
 
 
 DIPEInsertionSortSearch().search()
